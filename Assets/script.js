19c65cf194372a06658d3c8e25312fb8a50ed646
Kelsie Szost <kelsieszost@Kelsies-MacBook-Pro.local>
2022-08-21 17:58:33 -0400

    get weather uv index

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,9 @@
 var APIKey = "b3a39c328c85f0183f77ee0cb31af416";
 
 var cities = [];
-var currentCity;
+var selectedCity;
 
-var searchList = document.getElementById("list-of-cities");
+var searchList = document.getElementById(".history");
 var ciySearchHistory = JSON.parse(localStorage.getItem("cities")) || [];
 
 $(document).ready(function() {
@@ -26,7 +26,7 @@ $("#searchBtn").on("click", function(event){
 
     getWeather();
     storedCities();
-    showCity()
+    cityDisplay()
 ;}
 )
 
@@ -36,6 +36,26 @@ function getWeather(){
     fetch(weatherUrl)
     .then(function(response) {
         return response.json();
+    })
+
+    .then(function(data) {
+        currentDay = moment.unix(data.dt).format("l");
+        var temperature = data.main.temp;
+        temp = ((temperature - 273.15) * 1.80 + 32).toFixed(1);
+        windSpeed = data.wind.speed;
+        humidity = data.main.humidity;
+        var latitude = data.coord.lat;
+        var longitude = data.coord.lon;
+
+        var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + APIKey + "&lat=" +latitude + "&lon=" +longitude;
+        fetch(uvUrl)
+
+    .then(function(response){
+        return response.json
+    })
+    .then(function(data) {
+        uvId = data.value;
     }
     )
+    })
 }
