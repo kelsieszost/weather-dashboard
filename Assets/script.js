050d59e44d226582f3f8061c365fd6dd44bffd91
Kelsie Szost <kelsieszost@Kelsies-MacBook-Pro.local>
2022-08-21 17:29:43 -0400

    search button functionality

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,7 @@ var APIKey = "b3a39c328c85f0183f77ee0cb31af416";
 varcities = [];
 var currentCity;
 
-var tableBody = document.getElementById("list-of-cities");
+var searchList = document.getElementById("list-of-cities");
 var ciySearchHistory = JSON.parse(localStorage.getItem("cities")) || [];
 
 $(document).ready(function() {
@@ -16,3 +16,13 @@ function storedCities() {
     localStorage.setItem("cities", JSON.stringify(cities));
     console.log(localStorage);
 }
+
+$("#searchBtn").on("click", function(event){
+    event.preventDefault();
+    city = $("#searchCities").preventDefault();
+
+    getWeather();
+    storedCities();
+    showCity()
+;}
+)
