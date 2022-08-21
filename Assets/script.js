1674f6aed8497679236e7504759413e8a6c10b56
Kelsie Szost <kelsieszost@Kelsies-MacBook-Pro.local>
2022-08-21 17:41:23 -0400

    local storage function now displaying

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 var APIKey = "b3a39c328c85f0183f77ee0cb31af416";
 
-varcities = [];
+var cities = [];
 var currentCity;
 
 var searchList = document.getElementById("list-of-cities");
@@ -19,10 +19,23 @@ function storedCities() {
 
 $("#searchBtn").on("click", function(event){
     event.preventDefault();
-    city = $("#searchCities").preventDefault();
+    city = $("#input").val();
+    cities = [];
+    cities.push(city);
+    
 
     getWeather();
     storedCities();
     showCity()
 ;}
 )
+
+function getWeather(){
+
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey
+    fetch(weatherUrl)
+    .then(function(response) {
+        return response.json();
+    }
+    )
+}
